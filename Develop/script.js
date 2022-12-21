ab0c4015ee2f095cf0cd681a826438641a8ef67b
jakester101 <55941541+jakester101@users.noreply.github.com>
2022-12-21 06:15:32 -0500

    added input validation during prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,58 @@
 // Assignment code here
 
+function inputCheck(input){
+  if(input !== 'yes' || input !== no){
+    alert('Please only enter yes or no.');
+    throw new Error();
+  }
+  if(typeof input !== 'string'){
+    alert('Invalid data type!');
+    throw new Error();
+  }
+
+  return;
+}
+
+function lengthCheck(size){
+  if(size < 8 || size > 128){
+    alert('Length must fit constraints');
+    throw new Error();
+  }
+  console.log(typeof size);
+  console.log(size);
+  if(isNaN(size)){
+    alert('Invalid data type!');
+    throw new Error();
+  }
+  return;
+}
+
+
+function generatePassword(){
+
+  var password = [];
+  var isLength = prompt('How long is your password?','Enter a number between 8 and 128');
+  isLength = parseInt(isLength);  //convert the string to int for validation
+  lengthCheck(isLength);
+  var isUpper = prompt('Does this password contain uppercase?','Enter yes or no');
+  inputCheck(isUpper);
+  var isLower = prompt('Does this password contain lowercase?','Enter yes or no');
+  inputCheck(isLower);
+  var isNumbers = prompt('Does this password contain numbers?','Enter yes or no');
+  inputCheck(isNumbers);
+  var isSpecial = prompt('Does this password contain special characters?','Enter yes or no');
+  inputCheck(isSpecial);
+
+
+
+
+
+  return text; //whatever is returned will be the complete password displayed on screen
+
+}
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
